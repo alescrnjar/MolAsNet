43c168841c0d01e0141bc3c0e110db32d3aac8ee
alescrnjar <a.crnjar@gmail.com>
2023-03-02 14:13:39 +0100

    made debugs

--- MolAsNet.py
+++ MolAsNet.py
@@ -7,16 +7,16 @@ import numpy as np
 import torch
 from sklearn.preprocessing import StandardScaler
 
-import argparse #AC
-from tensorboardX import SummaryWriter #AC
-from protein_graph_data import * #AC
-from plots_for_gnn import * #AC
+import argparse 
+from tensorboardX import SummaryWriter 
+from protein_graph_data import * 
+from plots_for_gnn import * 
 from beta_rate import *
 
 parser = argparse.ArgumentParser()
 # Input parameters
 parser.add_argument('--dset', default='MolAsNet', type=str) 
-parser.add_argument('--input_directory', default='./example_input', type=str)
+parser.add_argument('--input_directory', default='./example_input/', type=str)
 parser.add_argument('--pdbid', default='6eqe', type=str) # PDB ID of input protein
 
 # Model parameters
@@ -30,7 +30,7 @@ parser.add_argument('--random_seed', default=42, type=int)
 
 # Output parameters
 parser.add_argument('--log_freq', default=100, type=int) # Frequency for output
-parser.add_argument('--output_directory', default='./', type=str) 
+parser.add_argument('--output_directory', default='./example_output/', type=str) 
 
 args = parser.parse_args()
 print(f"{args=}")
@@ -39,28 +39,28 @@ def labeling(my_list):
     """
     Turn list into new numerical list.
     """
-    new_list=[]
-    unique=list(np.unique(my_list))
+    new_list = []
+    unique = list(np.unique(my_list))
     for x in my_list:
         new_list.append(unique.index(x))
     return new_list
 
 if args.dset=='MolAsNet':
-    G=protein_graph(args.pdbid+'_protein.mol2')
+    G=protein_graph(args.input_directory+args.pdbid+'_protein.mol2')
 
-    species=[]
-    is_H=[]
-    resnames=[]
-    in_backbone=[]
+    species = []
+    is_H = []
+    resnames = []
+    in_backbone = []
     for i,node in enumerate(G.nodes):
         is_H.append(G.nodes[node]['is_H'])
         species.append(G.nodes[node]['species'])
         resnames.append(G.nodes[node]['resname'])
         in_backbone.append(G.nodes[node]['in_backbone'])
     
-    if args.classification=='is_H': labels=labeling(is_H)
-    if args.classification=='species': labels=labeling(species)
-    if args.classification=='in_backbone': labels=labeling(in_backbone)
+    if args.classification=='is_H': labels = labeling(is_H)
+    if args.classification=='species': labels = labeling(species)
+    if args.classification=='in_backbone': labels = labeling(in_backbone)
     labels=np.asarray(labels).astype(np.int64)
     
     # Make COO-format edges
@@ -125,21 +125,21 @@ def data_statistics(data,what_mask='test_mask'):
     Print out train/test dataset statistics.
     """
     print("--- Statistics for:",what_mask)
-    species_counts={'C':0,'O':0,'N':0,'H':0,'S':0,'heavy':0}
-    atoms_per_resname={'ALA':0,'ARG':0,'CYS':0,'GLU':0,'GLN':0,'THR':0,'TYR':0,'VAL':0,'ILE':0,'LEU':0,'PRO':0,'MET':0,'ASN':0,'ASP':0,'LYS':0,'PHE':0,'GLY':0,'SER':0,'TRP':0,'HIS':0}
+    species_counts = {'C':0,'O':0,'N':0,'H':0,'S':0,'heavy':0}
+    atoms_per_resname = {'ALA':0,'ARG':0,'CYS':0,'GLU':0,'GLN':0,'THR':0,'TYR':0,'VAL':0,'ILE':0,'LEU':0,'PRO':0,'MET':0,'ASN':0,'ASP':0,'LYS':0,'PHE':0,'GLY':0,'SER':0,'TRP':0,'HIS':0}
     mask = data[what_mask]
     indeces=[]
     for j,tf in enumerate(mask):
-        if tf==True: 
-            atoms_per_resname[resnames[j]]+=1
+        if tf == True: 
+            atoms_per_resname[resnames[j]] += 1
     print(f"{atoms_per_resname=}")   
 
     
 if args.dset=='MolAsNet':
     dataset = MolDataset()
 data = dataset[0]
-data_statistics(data,what_mask='train_mask')
-data_statistics(data,what_mask='test_mask')
+data_statistics(data, what_mask='train_mask')
+data_statistics(data, what_mask='test_mask')
 
 #### Graph Convo Network
 
@@ -150,7 +150,7 @@ from torch_geometric.nn import GCNConv
 # GCN model
 class GCN_Net(torch.nn.Module):
     def __init__(self):
-        super(Net3, self).__init__()
+        super(GCN_Net, self).__init__()
         self.conv1 = GCNConv(data.num_features, args.n_hidden)
         self.conv2 = GCNConv(args.n_hidden, args.n_hidden)
         self.conv3 = GCNConv(args.n_hidden, args.n_hidden)
@@ -200,7 +200,7 @@ def plot_dict(dict0,dict_std,features,max_feat=None,png_name='Rates.png'):
     """                                                                                                                                                                            
     Plot errorbars
     """
-    if max_feat==None: max_feat=len(dict0)
+    if max_feat == None: max_feat = len(dict0)
     fig = plt.figure(1, figsize=(4, 4))
     plt.rcParams.update({'font.size': 6.8})
     plt.errorbar(np.linspace(0,max_feat,max_feat),dict0[:max_feat],yerr=dict_std,fmt='none',elinewidth=1,ecolor='C0')
@@ -225,7 +225,7 @@ def results(mask, pred, indeces):
         if success_check: mysum+=1
         if i<4:
             print(f"{pr=} {indeces[i]=} {labels[indeces[i]]=} {species[indeces[i]]=} {is_H[indeces[i]]=} {resnames[indeces[i]]=} {success_check=}") # {list(labels).index[indeces[i]]=}")
-    set_beta(successes,inpname=args.pdbid+'_protein.mol2')
+    set_beta(successes,inpname=args.input_directory+args.pdbid+'_protein.mol2')
     acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
     print(f"{acc=}")
     print()
@@ -238,27 +238,27 @@ def results_per_sel(pred, indeces, what_sel='resnames'):
     if what_sel=='species': all_sel=['C','O','N','H','S']
     if what_sel=='resnames': all_sel=['ALA','ARG','CYS','GLU','GLN','THR','TYR','VAL','ILE','LEU','PRO','MET','ASN','ASP','LYS','PHE','GLY','SER','TRP','HIS']
     if what_sel=='in_backbone': all_sel=['backbone','side-chain']
-    counts_per_sel={}
-    succ_per_sel={}
-    succ2_per_sel={}
-    std_per_sel={}
+    counts_per_sel = {}
+    succ_per_sel = {}
+    succ2_per_sel = {}
+    std_per_sel = {}
     for sel in all_sel:
-        counts_per_sel[sel]=0
-        succ_per_sel[sel]=0.
-        succ2_per_sel[sel]=0.
-        std_per_sel[sel]=0.
+        counts_per_sel[sel] = 0
+        succ_per_sel[sel] = 0.
+        succ2_per_sel[sel] = 0.
+        std_per_sel[sel] = 0.
     for i,pr in enumerate(list(pred)):
-        success_check=int(pr)==labels[indeces[i]]
-        if what_sel=='species': sel=species[indeces[i]]
-        if what_sel=='resnames': sel=resnames[indeces[i]]
-        if what_sel=='in_backbone': sel=in_backbone[indeces[i]]
-        counts_per_sel[sel]+=1
-        succ_per_sel[sel]+=success_check
-        succ2_per_sel[sel]+=(success_check)**2
+        success_check= int(pr)==labels[indeces[i]]
+        if what_sel=='species': sel = species[indeces[i]]
+        if what_sel=='resnames': sel = resnames[indeces[i]]
+        if what_sel=='in_backbone': sel = in_backbone[indeces[i]]
+        counts_per_sel[sel] += 1
+        succ_per_sel[sel] += success_check
+        succ2_per_sel[sel] += (success_check)**2
     for sel in all_sel:
-        succ_per_sel[sel]/=counts_per_sel[sel]
-        succ2_per_sel[sel]/=counts_per_sel[sel]
-        std_per_sel[sel]=np.sqrt(succ2_per_sel[sel]-succ_per_sel[sel]*succ_per_sel[sel])
+        succ_per_sel[sel] /= counts_per_sel[sel]
+        succ2_per_sel[sel] /= counts_per_sel[sel]
+        std_per_sel[sel] = np.sqrt(succ2_per_sel[sel]-succ_per_sel[sel]*succ_per_sel[sel])
         print("Rate for {}: {} +- {}".format(sel,succ_per_sel[sel],std_per_sel[sel])) 
     plot_dict(list(succ_per_sel.values()),list(std_per_sel.values()),features=list(succ_per_sel.keys()),max_feat=None,png_name='Rates_per_'+what_sel+'_'+args.pdbid+'.png')
     
