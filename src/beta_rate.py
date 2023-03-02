ed162812c6198fc699b9014518cd32635f302fef
alescrnjar <a.crnjar@gmail.com>
2023-03-02 14:56:23 +0100

    removed comments

--- src/beta_rate.py
+++ src/beta_rate.py
@@ -5,11 +5,9 @@ def set_beta(successes,inpname='6eqe_protein.mol2'):
     u=mda.Universe(inpname.replace('mol2','pdb'))
     prot=u.select_atoms('protein')
     betas=[]
-    #for i_at,at in prot.atoms.ids:
     # https://www.mdanalysis.org/MDAnalysisTutorial/writing.html
     u.atoms.tempfactors = np.array(successes)
     outname=inpname.replace('.mol2','')+'_beta.pdb'
-    #with MDAnalysis.Writer(outname, multiframe=True, bonds=None, n_atoms=u.atoms.n_atoms) as PDB:
     with mda.Writer(outname) as PDB:
         PDB.write(u.atoms)
     print(outname+" written.")
